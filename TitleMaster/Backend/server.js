7578fa017ddf086fbf25f7b01383bbe06beb0983
Pickggum <164029298+PickGGum@users.noreply.github.com>
2025-09-08 16:19:44 +0900

    Update server.js

---
+++ TitleMaster/Backend/server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const GameManager = require("./gameManager");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const gameManager = new GameManager();
+
+io.on("connection", (socket) => {
+  console.log("새 유저 접속:", socket.id);
+
+  // 방 생성
+  socket.on("createRoom", ({ name }, callback) => {
+    const roomCode = gameManager.createRoom(socket.id, name);
+    socket.join(roomCode);
+    callback({ roomCode });
+    io.to(roomCode).emit("updatePlayers", gameManager.getRoom(roomCode).players);
+  });
+
+  // 방 참가
+  socket.on("joinRoom", ({ roomCode, name }, callback) => {
+    const success = gameManager.addPlayer(roomCode, socket.id, name);
+    if (!success) {
+      callback({ error: "방이 존재하지 않습니다." });
+      return;
+    }
+    socket.join(roomCode);
+    callback({ success: true });
+    io.to(roomCode).emit("updatePlayers", gameManager.getRoom(roomCode).players);
+  });
+
+  // 게임 시작
+  socket.on("startGame", ({ roomCode, maxRounds, titles }) => {
+    const firstTitle = gameManager.startGame(roomCode, maxRounds, titles);
+    io.to(roomCode).emit("gameStarted", { title: firstTitle });
+  });
+
+  // 투표 처리
+  socket.on("submitVotes", ({ roomCode, votes }) => {
+    const result = gameManager.calculateVotes(roomCode, votes);
+    io.to(roomCode).emit("voteResult", result);
+
+    // 다음 라운드로 진행
+    const nextTitle = gameManager.nextRound(roomCode);
+    if (nextTitle) {
+      io.to(roomCode).emit("nextRound", { title: nextTitle });
+    } else {
+      const room = gameManager.getRoom(roomCode);
+      io.to(roomCode).emit("gameEnded", { scores: room.players });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("연결 해제:", socket.id);
+    // TODO: 플레이어 제거 로직 추가 가능
+  });
+});
+
+server.listen(3000, () => {
+  console.log("서버 실행 중: http://localhost:3000");
+});
